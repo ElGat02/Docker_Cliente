fe223d1d4f5c1e0c9460f984f7d6e7746911a5a8
ElGato02 <crepy27@gmail.com>
2024-05-16 17:52:31 -0500

    ppp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './clients/add-client/add-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-client', component: AddClientComponent },
+  // otras rutas...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
