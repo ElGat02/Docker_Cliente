a69ff281d05e2f6920fe874f1113638066dcf405
ElGato02 <crepy27@gmail.com>
2024-05-16 17:29:58 -0500

    xd

---
+++ src/app/Model/Client.model.ts
@@ -0,0 +1,13 @@
+export class Client {
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+
+  constructor(name: string, email: string, phone: string, id?: number) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './Pages/client/client.component';
+import { AddClientComponent } from './clients/add-client/add-client.component';
+import { EditClientComponent } from './clients/edit-client/edit-client.component';
+import { ListClientsComponent } from './clients/list-clients/list-clients.component';
+import { Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'add-client', component: AddClientComponent },
+  { path: 'edit-client/:id', component: EditClientComponent },
+  { path: '', component: ListClientsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    AddClientComponent,
+    EditClientComponent,
+    ListClientsComponent
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule
   ],
